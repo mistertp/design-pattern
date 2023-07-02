f54182346def6d19df56a9f1938fd0f697d4b13b
mistertp <49952900+mistertp@users.noreply.github.com>
2023-07-02 22:49:17 +0800

    feat: add singleton pattern module

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# design-pattern
-Design pattern in action
+> **注意**：如果你想要阅读中文版`README`, 请点击 [这里](https://github.com/mistertp/design-pattern/README_ZH.md). 
+
+# Design pattern
+
+Design pattern in action. Code example using java.
+
+## Index
+
+- [Creational patterns](#Creational patterns)
+- [Structural patterns](#Structural patterns)
+- [Behavioral patterns](#Behavioral patterns)
+
+## Creational patterns
+
+These patterns provide various object creation mechanisms, which increase flexibility and reuse of existing code.
+
+## Structural patterns
+
+These patterns explain how to assemble objects and classes into larger structures while keeping these structures flexible and efficient.
+
+## Behavioral patterns
+
+These patterns are concerned with algorithms and the assignment of responsibilities between objects.
---
+++ creational/singleton/src/main/java/org/mistertp/singleton/Singleton.java
@@ -0,0 +1,50 @@
+package org.mistertp.singleton;
+
+/**
+ * 经典单例模式（延迟实例化）
+ * 懒汉（线程不安全）
+ *
+ * @author mistertp
+ * @date 2023/7/2 下午10:34
+ */
+public class Singleton {
+
+    /**
+     * 定义单例全局变量
+     */
+    private static Singleton instance;
+
+    /**
+     * 私有化构造函数
+     */
+    private Singleton() {
+
+    }
+
+    /**
+     * 提供获取实例的公共静态方法
+     * <p>
+     * 这种写法起到了lazy loading的效果，即先判断实例是否存在，不存在的情况，再实例化。但是只能在单线程的情况下使用。
+     * 在多线程的情况下，假如A线程进入到if(instance == null), 但是B线程已经执行到 instance = new Singleton()， 这种情况下，就会产生产生多个实例，未达到单例的效果。
+     *
+     * @return Singleton
+     */
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+class Test {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton anotherSingleton = Singleton.getInstance();
+        System.out.println(singleton);
+        System.out.println(anotherSingleton);
+    }
+
+}
+
